2e318f55da1f226565b81c3225dce516084bd005
charlieorch <charlieorch1@gmail.com>
2021-04-21 11:29:58 -0500

    add TrackList component

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class TrackList extends React.Component {
+  render() {
+    return <div className="TrackList">{/* add tracks here */}</div>;
+  }
+}
+
+export default TrackList;
