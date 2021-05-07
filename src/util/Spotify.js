a5ea47df986f4f509e6bbc173082e826b3afe46e
charlieorch <charlieorch1@gmail.com>
2021-05-07 16:37:59 -0500

    implemented functionality to obtain a Spotify access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,6 @@
 let accessToken;
+let clientID = "8f15d92144ac450f9939d82993ad0d70";
+let redirectURI = "http://localhost:3000/";
 
 const Spotify = {
   getAccessToken() {
@@ -6,8 +8,21 @@ const Spotify = {
       return accessToken;
     }
 
-    const accessToken = window.location.href.match(/access_token=([^&]*)/);
-    const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    // checking to see if the access token is match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
+    }
   },
 };
 
